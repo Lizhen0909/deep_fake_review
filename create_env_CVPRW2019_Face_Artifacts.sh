bad94a11e08ca36407007c2b99c4085dbf7ec4c3
Lizhen Shi <lizhen9.shi@gmail.com>
2021-08-10 12:19:26 -0700

    Initial commit

---
+++ create_env_CVPRW2019_Face_Artifacts.sh
@@ -0,0 +1,4 @@
+conda create --channel conda-forge --name CVPRW2019_Face_Artifacts python=2.7  \
+&& conda activate CVPRW2019_Face_Artifacts \
+&& pip install  pyyaml==3.12 easydict==1.7 matplotlib==1.5.3 dlib==19.16.0 opencv-python==3.4.0.14 tensorflow==1.3.0
+
---
+++ create_env_headpose_forensic.sh
@@ -0,0 +1,5 @@
+conda create --channel conda-forge --name headpose_forensic python=2.7  \
+&& conda activate headpose_forensic \
+&& pip install  tqdm==4.28.1 numpy==1.15.4 dlib==19.16.0 opencv-python==3.4.3.18 scikit-learn==0.20.2 scipy==1.2.3
+
+
