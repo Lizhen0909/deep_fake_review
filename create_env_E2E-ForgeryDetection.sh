bed92c638c766a5c30b88e2ce3c841eff7e53716
Lizhen Shi <lizhen9.shi@gmail.com>
2021-08-11 20:02:10 -0700

    more tests

---
+++ create_env_E2E-ForgeryDetection.sh
@@ -0,0 +1,3 @@
+conda create --channel conda-forge --name E2E-ForgeryDetection python=3.5  -y
+conda run -n E2E-ForgeryDetection  pip install -r E2E-ForgeryDetection/requirements.txt
+
---
+++ create_env_ManTraNet.sh
@@ -0,0 +1,3 @@
+conda create --channel conda-forge -c pytorch --name ManTraNet python=3.7 
+conda run -n ManTraNet pip install Keras==2.3.0 TensorFlow==1.13.2
+
